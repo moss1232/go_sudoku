e6145696096c7dcb0f69a410d315d3d566ba1532
moss1232 <saito-to@belong.co.jp>
2022-10-11 21:41:59 +0900

    add pretty

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+type Board [9][9]int
+
+func pretty(b Board) string {
+	var buf = bytes.Buffer{}
+	for i := 0; i < 9; i++ {
+		if i%3 == 0 {
+			buf.WriteString("+---+---+---+\n")
+		}
+		for j := 0; j < 9; j++ {
+			if j%3 == 0 {
+				buf.WriteString("|")
+			}
+			buf.WriteString(strconv.Itoa(b[i][j]))
+		}
+		buf.WriteString("|\n")
+	}
+	buf.WriteString("+---+---+---+")
+	return buf.String()
+}
+
+func main() {
+	b := Board{
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	fmt.Print(pretty(b))
+}
