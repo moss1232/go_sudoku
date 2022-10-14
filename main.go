419d4b7e2c5f5cc0a9757cb4806f086d2d03b4fc
moss1232 <saito-to@belong.co.jp>
2022-10-14 22:19:02 +0900

    add func for solving

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"strconv"
 )
 
+// 0: 未入力
+// 1-9: 入力済み
+
 type Board [9][9]int
 
 func pretty(b Board) string {
@@ -26,6 +29,95 @@ func pretty(b Board) string {
 	return buf.String()
 }
 
+func duplicated(c [10]int) bool {
+	for k, v := range c {
+		if k == 0 {
+			continue
+		}
+		if v >= 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func verify(b Board) bool {
+	// check row
+	for i := 0; i < 9; i++ {
+		// check column
+		var c [10]int
+		for j := 0; j < 9; j++ {
+			c[b[i][j]]++
+		}
+		if duplicated(c) {
+			return false
+		}
+	}
+	// check column
+	for i := 0; i < 9; i++ {
+		// check column
+		var c [10]int
+		for j := 0; j < 9; j++ {
+			c[b[j][i]]++
+		}
+		if duplicated(c) {
+			return false
+		}
+	}
+	// check block
+	for i := 0; i < 9; i += 3 {
+		for j := 0; j < 9; j += 3 {
+			var c [10]int
+			for row := i; row < i+3; row++ {
+				for col := j; col < j+3; col++ {
+					c[b[row][col]]++
+				}
+			}
+			if duplicated(c) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func solved(b Board) bool {
+	if !verify(b) {
+		return false
+	}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if b[i][j] == 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func backtrack(b *Board) bool {
+	if solved(*b) {
+		return true
+	}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if b[i][j] == 0 {
+				for c := 9; c >= 1; c-- {
+					b[i][j] = c
+					if verify(*b) {
+						if backtrack(b) {
+							return true
+						}
+					}
+					b[i][j] = 0
+				}
+				return false
+			}
+		}
+	}
+	return false
+}
+
 func main() {
 	b := Board{
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -38,5 +130,5 @@ func main() {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 	}
-	fmt.Print(pretty(b))
+	fmt.Printf("%+v\n", pretty(b))
 }
